da5c4a4973f1e37fa140bda4cf7743245ea5ddf6
Gahana-ai <gahanajain20@gmail.com>
2026-07-25 13:04:53 +0530

    Fix Cannot GET / error by adding root route

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api/recipes', recipeRoutes);
+app.get('/', (req, res) => {
+  res.status(200).send('API is running... Use /api/recipes to fetch data.');
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
